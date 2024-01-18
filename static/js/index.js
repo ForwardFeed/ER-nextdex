b07460cc01443013948f3967d0bc9e314b8b61de
ForwardFeed <76596109+ForwardFeed@users.noreply.github.com>
2024-01-18 13:47:39 +0100

    just a little trollin

--- static/js/index.js
+++ static/js/index.js
@@ -2,6 +2,7 @@ import setupPanels from "./sidebar.js"
 import { setupSpeciesSubPanel } from "./species_panel.js"
 import { setAvailableVersion } from "./data_version.js"
 import { setupSearch } from "./search.js"
+import { addTooltip } from "./utils.js"
 
 $(document).ready(function(){
     setupPanels()
@@ -9,4 +10,5 @@ $(document).ready(function(){
     setAvailableVersion()
     setupSearch()
     setupSettings()
+    addTooltip($('.main-title')[0], 'Berkay, the dex is up btw')
 })
