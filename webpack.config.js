9bae0594f4d51cf316f19334ef3dcbcba4e2fcb4
ForwardFeed <76596109+ForwardFeed@users.noreply.github.com>
2024-02-21 20:03:15 +0100

    another test because ios bugs again

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,8 @@ const path = require('path');
 module.exports = {
   entry: './static/js/index.js',
   output: {
-    filename: 'webpacked.min.js',
+    filename: './static/js/webpacked.min.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  target: ['web', 'es5'],
 };
