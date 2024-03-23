ac78314724edd7a2310ab7d687e5dd69f2439b5f
ForwardFeed <76596109+ForwardFeed@users.noreply.github.com>
2024-03-23 13:40:39 +0100

    fixed trainer map

--- src/compactify.ts
+++ src/compactify.ts
@@ -367,24 +367,7 @@ export function compactify(gameData: GameData): CompactGameData {
         })
         trainerT.push(key)
     })
-    compacted.trainers = compacted.trainers.sort(function(a, b){
-        const aOrder = gameData.trainerOrder.indexOf(a.name)
-        const bOrder = gameData.trainerOrder.indexOf(b.name)
-        if (aOrder == -1 && bOrder == -1) return 0
-        if (aOrder == -1) return 1
-        if (bOrder == -1) return -1
-        return aOrder - bOrder
-    })
-    /*gameData.trainerOrder.forEach((x, i)=>{
-        const indexT = trainernameT.indexOf(x)
-        if (indexT == -1) {
-            console.log('miss named in trainer order : ' + x)
-            return
-        }
-        const trainerOrdered = compacted.trainers.splice(i, 1)[0]
-        compacted.trainers.push(compacted.trainers.splice(i, 1, trainerOrdered)[0])
-    })*/
-    
+
     gameData.dataScripted.forEach((val) => {
         const mapName = val.name.replace(/_/g, ' ')
             .replace(/(?<=[a-z])(?=[A-Z])/g, ' ')
@@ -407,7 +390,14 @@ export function compactify(gameData: GameData): CompactGameData {
             if (!compacted.trainers[trainerT.indexOf(value)] || compacted.trainers[trainerT.indexOf(value)].map == undefined) return
             compacted.trainers[trainerT.indexOf(value)].map = idMap
         })
-
+    })
+    compacted.trainers = compacted.trainers.sort(function(a, b){
+        const aOrder = gameData.trainerOrder.indexOf(a.name)
+        const bOrder = gameData.trainerOrder.indexOf(b.name)
+        if (aOrder == -1 && bOrder == -1) return 0
+        if (aOrder == -1) return 1
+        if (bOrder == -1) return -1
+        return aOrder - bOrder
     })
     compacted.locations = {
         landRate: gameData.locations.landRate,
