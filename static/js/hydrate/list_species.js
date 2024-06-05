3a7b29224b9684e5be94e2325ee073a389c1dba5
ForwardFeed <76596109+ForwardFeed@users.noreply.github.com>
2024-06-05 22:53:30 +0200

    large css fixes

--- static/js/hydrate/list_species.js
+++ static/js/hydrate/list_species.js
@@ -1,4 +1,5 @@
 import { gameData } from "../data_version.js";
+import { getSpritesURL } from "../panels/species/species_panel.js";
 import { JSHAC, e } from "../utils.js";
 
 
@@ -12,6 +13,7 @@ export function toggleLayoutList(toggle= true){
     }
 }
 
+
 export function hydrateSpeciesList(){
     const species = gameData.species
     const speciesLen = species.length
@@ -19,13 +21,30 @@ export function hydrateSpeciesList(){
     for (let i = 0; i < speciesLen; i++) {
         if (i == 0) continue // skip specie none
         const specie = species[i]
+        const nameRow = e('div', 'list-species-name')
+        /*row.setAttribute('draggable', true);
+        row.ondragstart = (ev) => {
+            ev.dataTransfer.setData("id", i)
+        }*/
+        //Node id because for correlation with nodelist in sorting
+        /*specie.nodeID = nodeLists.species.length
+        nodeLists.species.push(row)*/
+
+        const image = e('img', 'species-list-sprite list-species-sprite')
+        image.src = getSpritesURL(specie.NAME)
+        image.alt = specie.name
+        image.loading = "lazy"
+        nameRow.appendChild(image)
+
+        const name = e('span', "species-name span-a", specie.name)
+        nameRow.append(name)
         fragment.append(JSHAC([
             e('div', 'list-species-row'), [
-                e('div', 'list-species-name', [e('span', '', specie.name)]),
-                e('div', 'list-species-abis-block', specie.stats.abis.map(x => {
+                nameRow,
+                e('div', 'list-species-abis-block', [...new Set(specie.stats.abis)].filter(x => x).map(x => {
                     return e('div', 'list-species-abi', [e('span', null, gameData.abilities[x].name)])
                 })),
-                e('div', 'list-species-inns-block', specie.stats.inns.map(x => {
+                e('div', 'list-species-inns-block', [...new Set(specie.stats.inns)].filter(x => x).map(x => {
                     return e('div', 'list-species-inn', [e('span', null, gameData.abilities[x].name)])
                 })),
             ]
