77e388d0ab54c65b200c9f9159d81ff31563d475
ForwardFeed <76596109+ForwardFeed@users.noreply.github.com>
2024-06-23 12:24:24 +0200

    finaally fixed the scrolling, it took me days

--- static/js/dynamic_list.js
+++ static/js/dynamic_list.js
@@ -4,7 +4,7 @@
 
 import { nodeLists } from "./hydrate/hydrate.js"
 
-export const LIST_RENDER_RANGE = 20 //maximum that is loading on a
+export const LIST_RENDER_RANGE = 20 //maximum that is loading on a direction
 const RATE_LIMIT_INTERVAL = 400 // trigger the rendering of the list with this minimun in ms
 
 export class DynamicList{
@@ -15,8 +15,9 @@ export class DynamicList{
     /** @type {string} */
     nodeListName
     lastNbScrolled = 0
-    unloadOffset = 0
     nbRowScrolled = 0
+    prevScroll = 0
+    leftovers = 0
     ranges = {
         curr: {
             min: 0,
@@ -36,17 +37,54 @@ export class DynamicList{
         this.nodeListName = nodeListName
         this.scrollDirAuto = scrollDirAuto
     }
+    getScroll(){
+        if (this.scrollDirAuto && document.body.clientHeight > document.body.clientWidth){
+            return this.node.scrollLeft
+        }
+        return this.node.scrollTop
+    }
+    setScroll(val){
+        if (this.scrollDirAuto && document.body.clientHeight > document.body.clientWidth){
+            return this.node.scrollLeft = val
+        }
+        return this.node.scrollTop = val
+    }
+    getDivPx(){
+        // turns out that the reorder bar is always there and in the right size
+        // however this will not work well at all if there's no top bar, not good design but I don't plan to export it
+        if (this.scrollDirAuto && document.body.clientHeight > document.body.clientWidth){
+            return this.topBar.clientWidth
+        }
+        return this.topBar.clientHeight
+    }
+    getMaxSpace(){
+        if (this.scrollDirAuto && document.body.clientHeight > document.body.clientWidth){
+            return this.node.scrollLeftMax
+        }
+        return this.node.scrollTopMax
+    }
+    onScroll(){
+        const currScroll = this.getScroll()
+        this.leftovers += currScroll - this.prevScroll
+        this.prevScroll = currScroll
+        const pixelPerBlock = this.getDivPx()
+        const pixelMod = this.leftovers / pixelPerBlock
+        if (!pixelMod) return
+
+        const nbRowScrolled = pixelMod > 0 ?
+            Math.floor(this.leftovers / pixelPerBlock) :
+            Math.ceil(this.leftovers / pixelPerBlock)
+        this.leftovers = this.leftovers % pixelPerBlock
+        if (!nbRowScrolled) return
+        this.lastNbScrolled = this.nbRowScrolled
+        this.nbRowScrolled += nbRowScrolled
+        fastdom.mutate(() => {
+            this.update(nbRowScrolled)
+        })
+    }
     setup(){
-        let timeout
         this.node.onscroll = () => {
-            if (timeout) return
-            timeout = setTimeout(()=>{
-                fastdom.mutate(() => {
-                    this.update()
-                })
-                timeout = undefined
-            }, RATE_LIMIT_INTERVAL)  
-            
+            this.onScroll()
         }
     }
     dataUpdate(data){
@@ -54,65 +92,40 @@ export class DynamicList{
         return this
     }
     calculateRenderingRange(){
-        const scrollOrientation = {
-            size: "clientHeight",
-            dir: "scrollTop"
-        }
-        if (this.scrollDirAuto && document.body.clientHeight > document.body.clientWidth){
-            // vertical layout needs to add
-            scrollOrientation.size = "clientWidth"
-            scrollOrientation.dir = "scrollLeft"
-        }
-        // turns out that the reorder bar is always there and in the right size
-        // however it is not this will not work well at all, not good design but I don't plan to export it
-        const oneRowHeightPx =  this.topBar[scrollOrientation.size]
-        if (!oneRowHeightPx) return
-        const nbRowScrolledFloat = this.node[scrollOrientation.dir] / oneRowHeightPx
         const maxRow = this.data.length
-        const nbRowScrolledRaw = Math.min(maxRow, Math.round(nbRowScrolledFloat) + this.unloadOffset)
-        // Minus one because it takes in account the top reordering bar
-        this.nbRowScrolled = Math.max(0, nbRowScrolledRaw - 1)
         this.ranges = {
             curr: {
                 min: Math.max(0, this.nbRowScrolled - LIST_RENDER_RANGE),
                 max: Math.min(maxRow, this.nbRowScrolled + LIST_RENDER_RANGE)
             },
-            prev: {
-                min: Math.max(0, this.lastNbScrolled - LIST_RENDER_RANGE),
-                max: Math.min(maxRow, this.lastNbScrolled + LIST_RENDER_RANGE)
-            }
+            prev: this.ranges.curr
         }
+        //console.log(this.ranges.curr, this)
     }
     update(){
         this.calculateRenderingRange()
+        //console.log(this.nbRowScrolled, this.ranges.curr.min, this.ranges.curr.max)
         // first hide those out of range
         if (this.nbRowScrolled && (this.nbRowScrolled == this.lastNbScrolled)) return
         if (this.nbRowScrolled > this.lastNbScrolled){//scrolled down
-            for (let i = this.ranges.prev.min; i < this.ranges.curr.min; i++){
+            const len = this.ranges.curr.min
+            for (let i = this.ranges.prev.min; i < len; i++){
                 if (!this.renderNextRow(i, false)) break
             }
-            this.unloadOffset += this.ranges.curr.min - this.ranges.prev.min
         } else if (this.nbRowScrolled < this.lastNbScrolled){ //scrolled up
-            for (let i = this.ranges.prev.max; i > this.ranges.curr.max; i--){
+            const len = this.ranges.curr.max
+            for (let i = this.ranges.prev.max; i > len; i--){
                 if (!this.renderNextRow(i, false)) break
-            }
-            const scrollDiff = this.lastNbScrolled - this.nbRowScrolled
-            this.unloadOffset = Math.max(0, this.unloadOffset - scrollDiff)
-            if (scrollDiff > LIST_RENDER_RANGE) {
-                // dumb workaround but it works for me so far
-                if (this.node.clientHeight < this.node.clientWidth){
-                    this.node.scrollLeft = this.topBar.clientWidth * (LIST_RENDER_RANGE / 2)
-                } else {
-                    this.node.scrollTop = this.topBar.clientHeight * (LIST_RENDER_RANGE / 2)
-                }
             }
         }
-        
+        const unloadOffset = this.ranges.curr.min - this.ranges.prev.min
+        this.leftovers += unloadOffset * this.getDivPx()
         // then show those in range
-        for (let i = this.ranges.curr.min; i < this.ranges.curr.max; i++){
+        const len = this.ranges.curr.max
+        for (let i = this.ranges.curr.min; i < len; i++){
             if (!this.renderNextRow(i, true)) break
         }
-        this.lastNbScrolled = this.nbRowScrolled
+        
         return this
        
     }
@@ -136,7 +149,6 @@ export class DynamicList{
     }
     reset(){
         this.lastNbScrolled = 0
-        this.unloadOffset = 0
         return this
     }
     replaceList(callbackNodes, callbackDataUpdate){
