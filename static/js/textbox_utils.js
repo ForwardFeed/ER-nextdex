db6349a148738923e136ac91e0740517a687abb1
ForwardFeed <76596109+ForwardFeed@users.noreply.github.com>
2024-09-20 22:58:57 +0200

    started to work on an util for the future dev

---
+++ static/js/textbox_utils.js
@@ -0,0 +1,306 @@
+const latinFont = {
+    "maxLineLength": 208,
+    "numLines": 2,
+    "cursorOverlapWidth": 0,
+    "widths": {
+        " ": 3,
+        "À": 6,
+        "Á": 6,
+        "Â": 6,
+        "Ç": 6,
+        "È": 6,
+        "É": 6,
+        "Ê": 6,
+        "Ë": 6,
+        "Ì": 6,
+        "Î": 6,
+        "Ï": 6,
+        "Ò": 6,
+        "Ó": 6,
+        "Ô": 6,
+        "Œ": 8,
+        "Ù": 6,
+        "Ú": 6,
+        "Û": 6,
+        "Ñ": 6,
+        "ß": 6,
+        "à": 6,
+        "á": 6,
+        "ç": 6,
+        "è": 6,
+        "é": 6,
+        "ê": 6,
+        "ë": 6,
+        "ì": 6,
+        "î": 6,
+        "ï": 6,
+        "ò": 6,
+        "ó": 6,
+        "ô": 6,
+        "œ": 8,
+        "ù": 6,
+        "ú": 6,
+        "û": 6,
+        "ñ": 6,
+        "º": 6,
+        "ª": 6,
+        "{SUPER_ER}": 9,
+        "&": 7,
+        "+": 6,
+        "LV": 10,
+        "=": 8,
+        ";": 3,
+        "¿": 6,
+        "¡": 4,
+        "{PK}": 8,
+        "{PKMN}": 16,
+        "{POKEBLOCK}": 35,
+        "Í": 6,
+        "%": 6,
+        "(": 4,
+        ")": 4,
+        "â": 6,
+        "í": 6,
+        "{UNK_SPACER}": 6,
+        "{UP_ARROW}": 7,
+        "{DOWN_ARROW}": 7,
+        "{LEFT_ARROW}": 7,
+        "{RIGHT_ARROW}": 7,
+        "{SUPER_E}": 6,
+        "<": 6,
+        ">": 6,
+        "{SUPER_RE}": 8,
+        "0": 6,
+        "1": 6,
+        "2": 6,
+        "3": 6,
+        "4": 6,
+        "5": 6,
+        "6": 6,
+        "7": 6,
+        "8": 6,
+        "9": 6,
+        "!": 4,
+        "?": 6,
+        ".": 3,
+        "-": 6,
+        "·": 3,
+        "…": 6,
+        "“": 6,
+        "”": 6,
+        "‘": 3,
+        "'": 3,
+        "♂": 6,
+        "♀": 6,
+        "¥": 6,
+        ",": 3,
+        "×": 7,
+        "/": 6,
+        "A": 6,
+        "B": 6,
+        "C": 6,
+        "D": 6,
+        "E": 6,
+        "F": 6,
+        "G": 6,
+        "H": 6,
+        "I": 6,
+        "J": 6,
+        "K": 6,
+        "L": 6,
+        "M": 6,
+        "N": 6,
+        "O": 6,
+        "P": 6,
+        "Q": 6,
+        "R": 6,
+        "S": 6,
+        "T": 6,
+        "U": 6,
+        "V": 6,
+        "W": 6,
+        "X": 6,
+        "Y": 6,
+        "Z": 6,
+        "a": 6,
+        "b": 6,
+        "c": 6,
+        "d": 6,
+        "e": 6,
+        "f": 6,
+        "g": 6,
+        "h": 6,
+        "i": 4,
+        "j": 5,
+        "k": 6,
+        "l": 4,
+        "m": 6,
+        "n": 6,
+        "o": 6,
+        "p": 6,
+        "q": 6,
+        "r": 5,
+        "s": 6,
+        "t": 6,
+        "u": 6,
+        "v": 6,
+        "w": 6,
+        "x": 6,
+        "y": 6,
+        "z": 6,
+        "▶": 8,
+        ":": 5,
+        "Ä": 6,
+        "Ö": 6,
+        "Ü": 6,
+        "ä": 6,
+        "ö": 6,
+        "ü": 6,
+        "{TALL_PLUS}": 0,
+        "{PLAYER}": 56,
+        "{STR_VAR_1}": 80,
+        "{STR_VAR_2}": 80,
+        "{STR_VAR_3}": 80,
+        "{KUN}": 0,
+        "{RIVAL}": 56,
+        "{VERSION}": 56,
+        "{AQUA}": 24,
+        "{MAGMA}": 30,
+        "{ARCHIE}": 36,
+        "{MAXIE}": 30,
+        "{KYOGRE}": 36,
+        "{GROUDON}": 42,
+        "$": 0
+    }
+}
+const latinFontSmall = {
+    widths: {"0":5,"1":5,"2":5,"3":5,"4":5,"5":5,"6":5,"7":5,"8":5,"9":5," ":3,"À":5,"Á":5,"Â":5,"Ç":5,"È":5,"É":5,"Ê":5,"Ë":5,"Ì":4,"Î":4,"Ï":4,"Ò":5,"Ó":5,"Ô":5,"Œ":8,"Ù":5,"Ú":5,"Û":5,"Ñ":5,"ß":6,"à":5,"á":5,"ç":5,"è":5,"é":5,"ê":5,"ë":5,"ì":4,"î":4,"ï":4,"ò":5,"ó":5,"ô":5,"œ":8,"ù":5,"ú":5,"û":5,"ñ":5,"º":5,"ª":6,"{SUPER_ER}":9,"&":6,"+":6,"LV":8,"=":8,";":3,"¿":5,"¡":4,"Í":4,"%":6,"(":4,")":4,"â":5,"í":4,"{UP_ARROW}":7,"{DOWN_ARROW}":7,"{LEFT_ARROW}":7,"{RIGHT_ARROW}":7,"{SUPER_E}":5,"<":6,">":6,"{SUPER_RE}":8,"!":4,"?":5,".":3,"-":5,"·":3,"…":5,"“":5,"”":5,"‘":3,"'":3,"♂":5,"♀":5,"¥":6,",":3,"×":6,"/":6,"A":5,"B":5,"C":5,"D":5,"E":5,"F":5,"G":5,"H":5,"I":4,"J":5,"K":5,"L":5,"M":5,"N":5,"O":5,"P":5,"Q":5,"R":5,"S":5,"T":5,"U":5,"V":5,"W":5,"X":5,"Y":4,"Z":5,"a":5,"b":5,"c":5,"d":5,"e":5,"f":5,"g":5,"h":5,"i":4,"j":5,"k":5,"l":4,"m":5,"n":5,"o":5,"p":5,"q":5,"r":5,"s":5,"t":5,"u":5,"v":5,"w":5,"x":5,"y":5,"z":5,"▶":8,":":3,"Ä":5,"Ö":5,"Ü":5,"ä":5,"ö":5,"ü":5}
+}
+
+
+function createCFontedLines(text, nLines, nPixels, smallfont = false){
+    const font = smallfont ? latinFontSmall : latinFont
+    const chars = text.split('')
+    let charsLen = chars.length
+    let expandedIndexStart = 0
+    let expansion = false
+    let expanded = ""
+    for (let i = 0; i < charsLen; i++){
+        const char = chars[i]
+        if (!expansion && char === "{") {
+            expansion = true
+            expanded = "{"
+            expandedIndexStart = i
+            continue
+        }
+        if (expansion && char === "}"){
+            expansion = false
+            expanded += "}"
+            chars.splice(expandedIndexStart, i - expandedIndexStart + 1, expanded)
+            expandedIndexStart = 0
+            i = expandedIndexStart
+            charsLen = charsLen - expanded.length + 1
+            continue
+        } 
+        if (expansion){
+            expanded += char
+        }
+    }
+    const words = []
+    let word = ""
+    let wordPixel = 0
+    let acc = 0
+    for (let charIndex = 0; charIndex < charsLen; charIndex++){
+        const char = chars[charIndex]
+        if (char == "\\"){
+            const nextChar = chars[++charIndex]
+            switch(nextChar){
+                case "p":
+                    words.push({
+                        word  : word,
+                        length: wordPixel,
+                        endl  : true,
+                    })
+                    word = ""
+                    wordPixel = 0
+                    break
+                case "n":
+
+                    break
+                default:
+                    warn(`char unknown : \\${nextChar}`)
+                    continue
+            }
+        }
+        const charPxSize = font.widths[char]
+        acc += charPxSize
+        if (charPxSize === undefined){
+            warn(`char size unknow : "${char}"`)
+            continue
+        }
+        if (char === " "){
+            words.push({
+                word  : word,
+                length: wordPixel
+            })
+            word = ""
+            wordPixel = 0
+        } else {
+            word        += char
+            wordPixel   += charPxSize
+        }
+        
+    }
+    if (word){
+        words.push({
+            word  : word,
+            length: wordPixel
+        })
+    }
+    const lines = []
+    let line = ""
+    let linePixels = 0
+    const SPACE_PIXEL = font.widths[" "]
+    for (const word of words){
+        if (linePixels + word.length > nPixels){
+            lines.push(line)
+            line = word.word
+            linePixels = word.length
+        } else {
+            if (line){
+                line += " " + word.word
+                linePixels += SPACE_PIXEL + word.length
+            } else {
+                line += word.word
+                linePixels += word.length
+            }
+            
+            
+        }
+    }
+    if (line){
+        lines.push(line)
+    }
+    if (nLines){
+        return lines.splice(0, nLines)
+    }
+    return lines.splice(0)
+}
+
+function warn(text){
+    console.warn(text)
+    $('#error-inputing').text(text)
+}
+
+function setupTextBox(){
+    $('#inputText').on('keyup', function(ev){
+        const val = $(this).val()
+        const lined = createCFontedLines(val, undefined, latinFont.maxLineLength, false)
+        console.log(lined)
+        $('#outputText').text(lined.join('\n'))
+    })
+}
+
+window.addEventListener('load', function() { 
+    setupTextBox()
+}, false);
