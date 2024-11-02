94c625d9bc51693bf8a3fae0a0dd9d9ec3a960cb
ForwardFeed <76596109+ForwardFeed@users.noreply.github.com>
2024-11-02 11:44:14 +0100

    updated data

--- postBuild/update_alpha.sh
+++ postBuild/update_alpha.sh
@@ -10,6 +10,6 @@ git checkout .
 git pull
 cd "${curr}"
 npm run build && npm run run -- -ip "${prjAlpha}" -sv 2 -rd -o Alphatest
-git commit -am "updated data" && git push && git check github-pages && git merge main && git push && git checkout main
+git commit -am "updated data" && git push && git checkout github-pages && git merge main && git push && git checkout main
 
 echo "updated alpha data OK"
