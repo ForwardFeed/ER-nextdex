411e32acbfb47176ece84e0a1bffc5d9ba083191
Mawootad <adinfinatioma@gmail.com>
2025-04-15 23:46:59 -0700

    Remove checks for unused files

--- devDexServer/src/file_list.ts
+++ devDexServer/src/file_list.ts
@@ -1,39 +1,14 @@
-
 // list of file to sparsely check out, these are the one the nextdex application will require
 
-export default{
-    list: [
-        'include/global.h',
-        'src/data/text/abilities.h',
-        'src/data/battle_moves.h', 
-        'src/data/text/move_descriptions.h',
-        'src/data/text/move_names.h',
-        'src/data/graphics/pokemon.h',
-        'src/data/pokemon_graphics/front_pic_table.h',
-        'src/data/items.h',
-        'src/data/text/item_descriptions.h',
-        'src/data/pokemon/pokedex_text.h',
-        'src/data/pokemon/pokedex_entries.h', 
-        'src/data/text/species_names.h',
-        'src/data/pokemon/base_stats.h',
-        'src/data/pokemon/evolution.h',
-        'src/data/pokemon/egg_moves.h',
-        'src/data/pokemon/level_up_learnsets.h',
-        'src/data/pokemon/level_up_learnset_pointers.h',
-        'src/data/pokemon/tmhm_learnsets.h',
-        'src/data/pokemon/tutor_learnsets.h',
-        'src/data/pokemon/form_species_tables.h',
-        'src/data/pokemon/form_species_table_pointers.h',
-        'src/data/graphics/pokemon.h',
-        'src/data/pokemon_graphics/front_pic_table.h',
-        'src/battle_setup.c',
-        'src/data/trainers.h',
-        'src/data/trainer_parties.h',
-        'include/constants/species.h',
-        'include/constants/moves.h',
-        'include/constants/battle_config.h',
-        'src/battle_util.c',
-        'src/data/wild_encounters.json',
-        //'data/maps/', // due to the slowness of the git script, this will not be doable
-    ]
-}
+export default {
+  list: [
+    "include/global.h",
+    "src/battle_setup.c",
+    "src/data/trainers.h",
+    "src/data/trainer_parties.h",
+    "include/constants/battle_config.h",
+    "src/battle_util.c",
+    "src/data/wild_encounters.json",
+    //'data/maps/', // due to the slowness of the git script, this will not be doable
+  ],
+};
