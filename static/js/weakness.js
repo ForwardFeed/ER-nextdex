e96860331281c926e19b539742e43a15e6bd5b87
ForwardFeed <76596109+ForwardFeed@users.noreply.github.com>
2026-01-27 18:18:12 +0100

    and probably the last fixed related to duplicate type

--- static/js/weakness.js
+++ static/js/weakness.js
@@ -139,7 +139,7 @@ export function abilitiesToAddedType(abis){
 export function getDefensiveCoverage(specie, abiID){
     const abisID = [specie.stats.abis[abiID], ...specie.stats.inns].filter(x => x)
     const abiNames = abisID.map(x => gameData.abilities[x].name)
-    const defTypes = [...new Set(specie.stats.types), abilitiesToAddedType(abisID)]
+    const defTypes = [...new Set(specie.stats.types, abilitiesToAddedType(abisID))]
         .filter(x => x != undefined)
         .map(x => gameData.typeT[x])
         .filter(x => x)
