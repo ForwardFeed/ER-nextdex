48e0d5fef90317b15e2955068649bdb5961f32e3
ForwardFeed <76596109+ForwardFeed@users.noreply.github.com>
2026-03-11 16:56:49 +0100

    added export palette as .PAL

--- paletteEditor/src/SideBar.vue
+++ paletteEditor/src/SideBar.vue
@@ -1,11 +1,24 @@
 <script lang="ts" setup>
-import { active_pal_data, current_pal_data, palette_target_id, type PalTarget } from './data';
+import { active_pal_data, current_pal_data, current_pokemon_palette_data, palette_target_id, type PalTarget } from './data';
 import SpriteSelection from './components/SpriteSelection.vue';
+import { palette_to_text } from './export_data';
 
 const target_control: PalTarget[] = [
     "regular",
     "shiny"
 ]
+
+function download_pal(){
+    const text  = palette_to_text(active_pal_data.value)
+    const name  = `${current_pokemon_palette_data.value.NAME}_${palette_target_id.value.toUpperCase()}`
+    const type  =  "text/plain"
+    const blob  = new Blob([text], { type })
+    const dummy = document.createElement('a')
+    
+    dummy.download  = name
+    dummy.href      = window.URL.createObjectURL(blob);
+    dummy.click()
+}
 
 </script>
 <template>
@@ -25,7 +38,13 @@ const target_control: PalTarget[] = [
             </template>
             <!-- <PaletteColor  v-if="rgba && id !== 0" :rgba :id/> -->
         </template>
-        
+    </div>
+    <div class="palette-export">
+        <div @click="download_pal">
+            <span>
+                Download as .PAL
+            </span>
+        </div>
     </div>
 </aside>
 </template>
@@ -34,11 +53,18 @@ aside{
     display: flex;
     flex-direction: column;
     background-color: rgb(123, 168, 38);
+    overflow-y: scroll;
+    scrollbar-width: none; /* Firefox */
+    -ms-overflow-style: none;  /* IE 10+ */
+    width: 64px;
+}
+aside::-webkit-scrollbar {
+    width: 0px;
+    background: transparent; /* make scrollbar transparent */
 }
 .palette-container{
     display: flex;
     background-color: rgb(76, 104, 23);
-    overflow-y: scroll;
     width: 64px;
     flex-wrap: wrap;
 }
@@ -46,12 +72,16 @@ aside{
     display: flex;
     flex-direction: column;
 }
-.palette-controls-target{
+.palette-controls-target, .palette-export{
     width: 100%;
     cursor: pointer;
     user-select: none;
 }
-.palette-controls-target:hover{
+.palette-export{
+    word-wrap: break-word;
+    font-size: 0.7em;
+}
+.palette-export:hover, .palette-controls-target:hover{
     background-color: aqua;
 }
 </style>
